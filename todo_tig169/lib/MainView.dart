993a34541376249a777f848cb8de28f74432084b
emeli <gusfolkem@student.gu.se>
2020-11-30 17:56:37 +0100

    cleaned the code, made widgets

--- todo_tig169/lib/MainView.dart
+++ todo_tig169/lib/MainView.dart
@@ -13,45 +13,13 @@ class MainView extends StatelessWidget {
           flexibleSpace: _appBarDecoration(),
           title: Text("TIG169 TODO"),
           centerTitle: true,
-          actions: [
-            PopupMenuButton(
-                onSelected: (value) {
-                  Provider.of<MyState>(context, listen: false)
-                      .setFilterBy(value);
-                },
-                itemBuilder: (context) => [
-                      PopupMenuItem(
-                        child: Text("All"),
-                        value: "All",
-                      ),
-                      PopupMenuItem(
-                        child: Text("Done"),
-                        value: "Done",
-                      ),
-                      PopupMenuItem(
-                        child: Text("Undone"),
-                        value: "Undone",
-                      )
-                    ])
-          ],
+          actions: [_popupMenuButton(context)],
         ),
         body: Consumer<MyState>(
           builder: (context, state, child) =>
               TodoList(_filterList(state.list, state.filterBy)),
         ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.deepPurple,
-          child: Icon(Icons.add),
-          onPressed: () async {
-            var newItem = await Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => SecondView(TodoItem(text: ""))));
-            if (newItem != null) {
-              Provider.of<MyState>(context, listen: false).addItem(newItem);
-            }
-          },
-        ));
+        floatingActionButton: _floatingActionButton(context));
   }
 
   Widget _appBarDecoration() {
@@ -65,6 +33,27 @@ class MainView extends StatelessWidget {
     );
   } //_appBarDecoration, decorates the appbar with an image
 
+  Widget _popupMenuButton(context) {
+    return PopupMenuButton(
+        onSelected: (value) {
+          Provider.of<MyState>(context, listen: false).setFilterBy(value);
+        },
+        itemBuilder: (context) => [
+              PopupMenuItem(
+                child: Text("All"),
+                value: "All",
+              ),
+              PopupMenuItem(
+                child: Text("Done"),
+                value: "Done",
+              ),
+              PopupMenuItem(
+                child: Text("Undone"),
+                value: "Undone",
+              )
+            ]);
+  } //popupMenuButton
+
   List<TodoItem> _filterList(_list, filterBy) {
     if (filterBy == "All") return _list;
     if (filterBy == "Done")
@@ -72,5 +61,21 @@ class MainView extends StatelessWidget {
     if (filterBy == "Undone")
       return _list.where((item) => item.check == false).toList();
     return _list;
+  }
+
+  Widget _floatingActionButton(context) {
+    return FloatingActionButton(
+      backgroundColor: Colors.deepPurple,
+      child: Icon(Icons.add),
+      onPressed: () async {
+        var newItem = await Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => SecondView(TodoItem(text: ""))));
+        if (newItem != null) {
+          Provider.of<MyState>(context, listen: false).addItem(newItem);
+        }
+      },
+    );
   }
 } //MainView
